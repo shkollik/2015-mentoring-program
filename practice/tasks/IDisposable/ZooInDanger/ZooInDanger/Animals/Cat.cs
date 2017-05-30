6acb21eff8fcf9f9aaeaf0602695874935be148a
Vladyslav Shkola <Vladyslav_Shkola@epam.com>
2017-05-30 19:19:15 +0300

    modified Cat.cs file in order to resolve issue with garbage collector

--- practice/tasks/IDisposable/ZooInDanger/ZooInDanger/Animals/Cat.cs
+++ practice/tasks/IDisposable/ZooInDanger/ZooInDanger/Animals/Cat.cs
@@ -22,6 +22,13 @@ namespace Zoo.Animals
         {
             Logger.LogYellow("Finalizing cat!");
 
+            //this check leads to issues with garbage collector
+            //after "Numcorpses" is more than 200 we can not finish this finalizer
+            //"Numcorpses" is decrementing in parent class -> Animal
+            //"Interlocked.Decrement(ref Zoo.NumCorpses);"
+
+            // the fix is just to skip this check
+            
             // Release the object only in case number of corpses > 100
             while (Zoo.NumCorpses > 200) { }
         }
